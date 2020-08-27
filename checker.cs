6dc7c6dcc9d5db24f4152d6c835dc791e5d977f5
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2020-08-27 21:50:50 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -4,11 +4,7 @@ using System.Diagnostics;
 class Checker
 {
     static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
+        if(spo2<90 || bpm < 70 || bpm > 150 || respRate < 30 || respRate >95){
             return false;
         }
         return true;
