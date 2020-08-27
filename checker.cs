54cb7b66c41a52182603db4fce615744ddb3217c
aayushvirkar <51874905+aayushvirkar@users.noreply.github.com>
2020-08-27 22:04:47 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -10,21 +10,10 @@ class Checker
       }
         return true;
     }
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
-    }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
-    }
+    
     static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
-        ExpectFalse(vitalsAreOk(40, 91, 92));
+        //ExpectTrue(vitalsAreOk(100, 95, 60));
+       // ExpectFalse(vitalsAreOk(40, 91, 92));
         Console.WriteLine("All ok");
         return 0;
     }
